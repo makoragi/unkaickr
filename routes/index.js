c6c1c34569c8af3e1f7769755a7e7bc4ae85b2f4
makoragi <makoragi@gmail.com>
2015-06-14 00:48:43 +0900

    API取得結果を表示

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+// var request = require('request');
+
+var Flickr = require("flickrapi"),
+	flickrOptions = {
+		api_key: process.env.api_key,
+		secret: process.env.secret
+	};
+
+var queryPhotoSearch = {
+	text: 'ラピュタの道',
+	per_page: '10',
+	format: 'json'
+};
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+		if (error) {
+			console.log(err);
+		} else {
+			flickr.photos.search(queryPhotoSearch, function (err, result) {
+				// result is Flickr's response
+				if (err) {
+					console.log(err);
+				} else {
+					// console.log(result.photos);
+					// var json = JSON.stringify(result.photos);
+					// var json = JSON.parse(result.photos);
+					res.render('index', {
+						title: 'Unkaickr - 雲海 x Flickr',
+						json: result.photos
+					});
+				}
+			});
+		}
+	});
 });
 
 module.exports = router;
